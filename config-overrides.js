58599cd52951f71a870251af235bf50295e2be9b
anton.backyard <anton.backyard@gmail.com>
2020-07-22 11:35:31 +0300

    docker admin

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,8 @@
-module.exports = function override(config, env) {
-    return config;
-};
+const {override, addWebpackPlugin} = require("customize-cra") ;
+const webpack = require('webpack')
+
+module.exports = override(
+    addWebpackPlugin(new webpack.DefinePlugin({
+        __DEV__: process.env.NODE_ENV !== 'production'
+    }))
+)
